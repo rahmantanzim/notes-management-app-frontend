b924103cf96b5408463a468eb046bb6b9470b0d8
Tanzim <tanzim008@gmail.com>
2025-05-13 07:45:49 +0600

    cretaed folderAndFile structure, created routes

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+import React from 'react'
+
+const Layout = () => { //this is going to be the parent component
+  return (
+    <Outlet/>
+  )
+}
+
+export default Layout
---
+++ src/features/auth/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Welcome = () => {
+    const date = new Date();
+    const today = new Intl.DateTimeFormat('bn-BD',{dateStyle :'full', timeStyle:'long'}).format(date);
+
+    const content = (
+        <section className='welcome'>
+            <p>{today}</p>
+            <h1>Welcome</h1>
+            <p></p>
+            <p><Link to='/dash/notes'>View Notes</Link></p>
+            <p><Link to='/dash/users'>View Users</Link></p>
+        </section>
+    )
+  return content;
+}
+
+export default Welcome
